e4b0609f323a1dba38dbe05a67c3dee93d3348b2
demetgas <demet_gas@hotmail.com>
2023-08-08 19:40:50 +0200

    packages installed

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import Tabs from './navigation/Tabs';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
 
 const App = () => {
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
-  )
-}
+  );
+};
 
-
-
-export default App
+export default App;
