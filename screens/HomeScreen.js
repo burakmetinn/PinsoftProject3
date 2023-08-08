07a1d355976c2520b4efcc0ac442dffad467fcb4
demetgas <demet_gas@hotmail.com>
2023-08-08 20:01:40 +0200

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
-import { View, Text, Button,  } from 'react-native'
-import React from 'react'
-import { StyleSheet } from 'react-native';
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
 
 const HomeScreen = () => {
   return (
@@ -8,16 +8,15 @@ const HomeScreen = () => {
       <Text>HomeScreen</Text>
       <Button style={styles.button} title="HomeScreen" />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  view:{
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center"
+  view: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
-})
+});
 
-
-export default HomeScreen
+export default HomeScreen;
