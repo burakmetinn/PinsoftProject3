2d5d785b3b137c96bdb717d28baa81ea0117699c
demetgas <demet_gas@hotmail.com>
2023-08-09 18:00:20 +0200

    Update LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,19 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, TextInput } from "react-native";
 import React from "react";
 import { StyleSheet } from "react-native";
 
 const LoginScreen = ({ navigation }) => {
   return (
     <View style={styles.view}>
-      <Text>LoginScreen</Text>
+      <Text style={styles.logo}>Time Off</Text>
+      <View>
+        <View style={styles.inputs}>
+          <TextInput placeholder="Email" />
+        </View>
+        <View>
+          <TextInput style={styles.inputs} placeholder="Password" />
+        </View>
+      </View>
       <Button
         title="Log in"
         onPress={() => {
@@ -27,7 +35,23 @@ const styles = StyleSheet.create({
   view: {
     flex: 1,
     alignItems: "center",
+    backgroundColor: "white",
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "black",
+    marginBottom: 50,
+    marginTop: 120,
+  },
+  inputs: {
+    width: 250,
+    backgroundColor: "#ebe8e8",
+    borderRadius: 20,
+    height: 60,
+    marginBottom: 20,
     justifyContent: "center",
+    padding: 20,
   },
 });
 
