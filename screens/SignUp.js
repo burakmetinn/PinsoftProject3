3709aceb0139a365120555c1cb4910efa108229e
demetgas <demet_gas@hotmail.com>
2023-08-10 15:00:57 +0200

    Update SignUp.js

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,12 +1,28 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import React from "react";
 import { StyleSheet } from "react-native";
 
 const SignUpScreen = () => {
   return (
     <View style={styles.view}>
-      <Text>Create an acoount</Text>
-      <Button style={styles.button} title="Sign Up" />
+      <Text style={styles.logo}>Sign Up</Text>
+      <View>
+        <View style={styles.inputs}>
+          <TextInput placeholder="Email" />
+        </View>
+        <View style={styles.inputs}>
+          <TextInput placeholder="Password" />
+        </View>
+      </View>
+      <View style={styles.btn}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Tabs");
+          }}
+        >
+          <Text style={styles.btnText}>Log In</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -15,7 +31,43 @@ const styles = StyleSheet.create({
   view: {
     flex: 1,
     alignItems: "center",
+    backgroundColor: "white",
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 30,
+    width: 115,
+    color: "black",
+    marginTop: 115,
+    marginBottom:50,
+    fontFamily: "Cochin-BoldItalic",
+  },
+  inputs: {
+    width: 250,
+    backgroundColor: "#ebeff2",
+    borderRadius: 20,
+    height: 60,
+    marginBottom: 20,
     justifyContent: "center",
+    padding: 20,
+  },
+  btn: {
+    padding: 20,
+    backgroundColor: "#e1e5e8",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+    width: 247,
+  },
+  btnText: {
+    fontWeight: "bold",
+  },
+  button: {
+    fontSize: 120,
+  },
+  btnText2: {
+    paddingTop: 15,
+    fontWeight: "bold",
   },
 });
 
