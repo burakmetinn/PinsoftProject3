27a7651c486ba265c8a620c3e22b8b13806a8d97
aylinnurercan <65025893+aylinnur@users.noreply.github.com>
2023-08-12 11:33:10 +0300

    permissionRequested

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 import Stacks from "./navigation/Stacks";
+import permissionRequested from "./requested/permissionRequested";
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stacks />
+      <permissionRequested/>
     </NavigationContainer>
   );
 };
