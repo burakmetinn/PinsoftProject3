ac62d3611ad9e13677ae7cfe14a23f20484a5612
demetgas <demet_gas@hotmail.com>
2023-08-12 14:50:52 +0200

    app updated

--- App.js
+++ App.js
@@ -2,13 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 import Stacks from "./navigation/Stacks";
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
 
->>>>>>> d97682843f8727859c491a0dcc00849da7fb0192
 const App = () => {
   return (
     <NavigationContainer>
