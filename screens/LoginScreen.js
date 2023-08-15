4b59999c5758bba5802f856cf62761a6434519e6
Omar Abdelhamed <omar792001@gmail.com>
2023-08-15 11:29:45 +0300

    password button fix

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,31 +6,31 @@ import {
   Image,
   Alert,
   ScrollView,
-} from "react-native";
-import React, { useRef, useState } from "react";
-import { StyleSheet } from "react-native";
-import Feather from "react-native-vector-icons/Feather";
-import Entypo from "react-native-vector-icons/Entypo";
+} from 'react-native';
+import React, { useRef, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const LoginScreen = ({ navigation }) => {
   const [hidePass, setHidePass] = useState(true);
-  const [textInputEmail, setTextInputEmail] = useState("");
-  const [textInputPwd, setTextInputPwd] = useState("");
+  const [textInputEmail, setTextInputEmail] = useState('');
+  const [textInputPwd, setTextInputPwd] = useState('');
 
   const checkTextInput = () => {
     if (!textInputEmail.trim()) {
-      Alert.alert("Error", "Please enter email.");
+      Alert.alert('Error', 'Please enter email.');
       return;
     }
     if (!textInputPwd.trim()) {
-      Alert.alert("Error", "Please enter password.");
+      Alert.alert('Error', 'Please enter password.');
       return;
     }
-    navigation.navigate("Tabs");
+    navigation.navigate('Tabs');
   };
   const pwd = useRef();
   return (
-    <ScrollView keyboardShouldPersistTaps="handled" style={styles.scroll}>
+    <ScrollView keyboardShouldPersistTaps='handled' style={styles.scroll}>
       <View style={styles.view}>
         <Text style={styles.logo}>Time Off</Text>
         <View>
@@ -40,17 +40,17 @@ const LoginScreen = ({ navigation }) => {
               height: 50,
               marginRight: 145,
               bottom: 45,
-              transform: [{ rotate: "300deg" }],
+              transform: [{ rotate: '300deg' }],
             }}
-            source={require("../assets/sun.png")}
+            source={require('../assets/sun.png')}
           />
         </View>
         <View>
           <View style={styles.inputs}>
             <TextInput
-              placeholder="Email"
+              placeholder='Email'
               style={{ top: 8 }}
-              returnKeyType="next"
+              returnKeyType='next'
               onSubmitEditing={() => {
                 pwd.current.focus();
               }}
@@ -58,26 +58,25 @@ const LoginScreen = ({ navigation }) => {
               onChangeText={(value) => setTextInputEmail(value)}
             />
             <Feather
-              name="mail"
-              style={{ fontSize: 15, left: 195, bottom: 7, color: "#999999" }}
+              name='mail'
+              style={{ fontSize: 15, left: 195, bottom: 7, color: '#999999' }}
             />
           </View>
           <View style={styles.inputs}>
             <TextInput
-              placeholder="Password"
+              placeholder='Password'
               style={{ top: 9 }}
               ref={pwd}
               secureTextEntry={hidePass ? true : false}
               onChangeText={(value) => setTextInputPwd(value)}
             />
-            <TouchableOpacity
-              onPress={() => {
-                setHidePass(!hidePass);
-              }}
-            >
+            <TouchableOpacity style={styles.lockButton}>
               <Entypo
-                name={hidePass ? "lock" : "lock-open"}
-                style={{ fontSize: 15, left: 195, bottom: 7, color: "#999999" }}
+                name={hidePass ? 'lock' : 'lock-open'}
+                style={styles.lockBtn}
+                onPress={() => {
+                  setHidePass(!hidePass);
+                }}
               />
             </TouchableOpacity>
           </View>
@@ -85,14 +84,14 @@ const LoginScreen = ({ navigation }) => {
         <View style={styles.btn}>
           <TouchableOpacity
             onPress={checkTextInput}
-            hitSlop={{ left: "100", right: "100", top: "20", bottom: "20" }}
+            hitSlop={{ left: 100, right: 100, top: 20, bottom: 20 }}
           >
             <Text style={styles.btnText}>Log In</Text>
           </TouchableOpacity>
         </View>
         <TouchableOpacity
           onPress={() => {
-            navigation.navigate("SignUpScreen");
+            navigation.navigate('SignUpScreen');
           }}
         >
           <Text style={styles.btnText2}>Don't have an account? Sign Up.</Text>
@@ -105,46 +104,59 @@ const LoginScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   view: {
     flex: 1,
-    alignItems: "center",
-    backgroundColor: "white",
+    alignItems: 'center',
+    backgroundColor: 'white',
   },
   scroll: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
   },
   logo: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     fontSize: 30,
     width: 115,
-    color: "black",
+    color: 'black',
     marginTop: 200,
-    fontFamily: "Cochin-BoldItalic",
+    fontFamily: 'Cochin-BoldItalic',
   },
   inputs: {
     width: 250,
-    backgroundColor: "#ebeff2",
+    backgroundColor: '#ebeff2',
     borderRadius: 20,
     height: 60,
     marginBottom: 20,
-    justifyContent: "center",
+    justifyContent: 'center',
     padding: 20,
   },
+
   btn: {
     padding: 20,
-    backgroundColor: "#e1e5e8",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: '#e1e5e8',
+    justifyContent: 'center',
+    alignItems: 'center',
     borderRadius: 20,
     width: 247,
   },
   btnText: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   button: {
     fontSize: 120,
   },
   btnText2: {
     paddingTop: 15,
-    fontWeight: "bold",
+    fontWeight: 'bold',
+  },
+  lockBtn: {
+    fontSize: 15,
+    left: 195,
+    bottom: 7,
+    color: '#999999',
+    width: 20,
+    marginLeft: 0,
+    padding: 0,
+  },
+  lockButton: {
+    width:20,
   },
 });
 
