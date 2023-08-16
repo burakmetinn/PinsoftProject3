f667b89999736e3b03ea8955457acdf1bccd9bba
demetgas <demet_gas@hotmail.com>
2023-08-16 13:21:57 +0200

    Update LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -24,7 +24,7 @@ const LoginScreen = ({ navigation }) => {
     else if (!textInputPwd.trim()) {
       Alert.alert("Error", "Please enter password.");
     }
-    else if (textInputEmail=="menager" && textInputPwd=="menager123") {
+    else if (textInputEmail=="manager" && textInputPwd=="manager123") {
       navigation.navigate("TabsManager");
     }
     else{
