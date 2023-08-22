15824293ab7d208cbd7b94074f520a86207ae727
demetgas <demet_gas@hotmail.com>
2023-08-22 13:57:57 +0200

    Update SelectAdminScreen.js

--- screens/SelectAdminScreen.js
+++ screens/SelectAdminScreen.js
@@ -1,8 +1,36 @@
 import React from "react";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
 import { Text } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { View } from "react-native-web";
 
 const SelectAdminScreen = ({ navigation }) => {
-  return <Text>Hello</Text>;
+  const [users, setUsers] = useState([
+    {
+      name: "John",
+      id: "1",
+    },
+    {
+      name: "Jake",
+      id: "2",
+    },
+    {
+      name: "Anna",
+      id: "3",
+    },
+  ]);
+  return (
+    <View>
+      <ScrollView>
+        {users.map((item) => (
+          <TouchableOpacity id={item.id}>
+            <Text>{item.name}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+  );
 };
 
 export default SelectAdminScreen;
