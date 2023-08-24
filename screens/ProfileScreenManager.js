181a1b9ccaf814805ecf733cc27493ea7c3e39b0
demetgas <demet_gas@hotmail.com>
2023-08-24 17:07:51 +0200

    Update ProfileScreenManager.js

--- screens/ProfileScreenManager.js
+++ screens/ProfileScreenManager.js
@@ -21,9 +21,11 @@ const ProfileScreenManager = ({ navigation }) => {
         <Text style={styles.sampleName}>Firstname Lastname</Text>
       </View>
 
-      <TouchableOpacity>
-        <Text>New Manager</Text>
-      </TouchableOpacity>
+      <View style={styles.newM}>
+        <TouchableOpacity>
+          <Text style={styles.new}>Select New Manager</Text>
+        </TouchableOpacity>
+      </View>
 
       <TouchableOpacity onPress={handleLogout} style={styles.logOutContainer}>
         <Ionicons name="log-out-outline" size={25} color="red" />
@@ -57,7 +59,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     justifyContent: "center",
     alignItems: "center",
-    color:"white"
+    color: "white",
   },
 
   logOutContainer: {
@@ -65,7 +67,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingVertical: 10,
     marginLeft: 30,
-    marginTop: 50,
+    marginTop: 380,
     width: 120,
   },
   logOutText: {
@@ -73,6 +75,21 @@ const styles = StyleSheet.create({
     color: "red",
     fontWeight: "bold",
     fontSize: 15,
+  },
+  newM: {
+    top: 50,
+    left: 30,
+    marginBottom: 10,
+    padding: 15,
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    width: 182,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  new: {
+    fontSize: 15,
+    color: "black",
   },
 });
 
